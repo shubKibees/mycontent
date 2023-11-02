a00979e39f668668a803e1a7c90c49fc214ef620
shubKibees <shubhamkibees@gmail.com>
2023-11-02 12:55:36 +0530

    added auto complete

--- jspractice/script_27.js
+++ jspractice/script_27.js
@@ -1,3 +1,58 @@
+var modelName=[{model:"Cartridge 912"},{model:"Cartridge 925"},{model:"Cartridge 303 TS"},{model:"Cartridge 047"},{model:"Drum 049"},{model:"Cartridge 050"},{model:"Drum 050"},{model:"Cartridge 308"},{model:"Cartridge 319"},{model:"Cartridge 319 II"},{model:"Cartridge 324"},{model:"Cartridge 324 II"},{model:"Cartridge 326"},{model:"Cartridge 337"},{model:"Cartridge 039"},{model:"Cartridge 039H"},{model:"Cartridge 041"},{model:"Cartridge 041H"},{model:"Cartridge 051"},{model:"Cartridge 051H"},{model:"Drum 051"},{model:"Cartridge 052"},{model:"Cartridge 052H"},{model:"Cartridge 057"},{model:"Cartridge 057H"},{model:"Cartridge 056L"},{model:"Cartridge 056"},{model:"Cartridge 056H"},{model:"Cartridge FX9"},{model:"Cartridge 320"},{model:"Cartridge 328"},{model:"Cartridge 328 VP"},{model:"Cartridge 071"},{model:"Cartridge 071 H"},{model:"Cartridge 073L"},{model:"Cartridge 073"},{model:"Cartridge 309"},{model:"Cartridge 333"},{model:"Cartridge 333 H"},{model:"Cartridge 316 Bk"},{model:"Cartridge 316 C"},{model:"Cartridge 316 M"},{model:"Cartridge 316 Y"},{model:"Cartridge 329 BK"},{model:"Cartridge 329 C"},{model:"Cartridge 329 M"},{model:"Cartridge 329 Y"},{model:"Drum Cartridge 029"},{model:"Cartridge 318 Bk"},{model:"Cartridge 318 C"},{model:"Cartridge 318 M"},{model:"Cartridge 318 Y"},{model:"Cartridge 331 Bk"},{model:"Cartridge 331 C"},{model:"Cartridge 331 M"},{model:"Cartridge 331 Y"},{model:"Cartridge 332 Bk"},{model:"Cartridge 040 Bk"},{model:"Cartridge 040 C"},{model:"Cartridge 040 M"},{model:"Cartridge 040 Y"},{model:"Cartridge 040H Bk"},{model:"Cartridge 040H C"},{model:"Cartridge 040H M"},{model:"Cartridge 040H Y"},{model:"Cartridge 045 BK"},{model:"Cartridge 045 C"},{model:"Cartridge 045 M"},{model:"Cartridge 045 Y"},{model:"Cartridge 045H BK"},{model:"Cartridge 045H C"},{model:"Cartridge 045H M"},{model:"Cartridge 045H Y"},{model:"Cartridge 046 BK"},{model:"Cartridge 046 C"},{model:"Cartridge 046 M"},{model:"Cartridge 046 Y"},{model:"Cartridge 046H BK"},{model:"Cartridge 046H C"},{model:"Cartridge 046H M"},{model:"Cartridge 046H Y"},{model:"Cartridge 054 BK"},{model:"Cartridge 054 C"},{model:"Cartridge 054 M"},{model:"Cartridge 054 Y"},{model:"Cartridge 054H BK"},{model:"Cartridge 054H C"},{model:"Cartridge 054H M"},{model:"Cartridge 054H Y"},{model:"Cartridge 055 BK"},{model:"Cartridge 055 C"},{model:"Cartridge 055 M"},{model:"Cartridge 055 Y"},{model:"Cartridge 055H BK"},{model:"Cartridge 055H C"},{model:"Cartridge 055H M"},{model:"Cartridge 055H Y"},{model:"Cartridge 416 BK"},{model:"Cartridge 416 C"},{model:"Cartridge 416 M"},{model:"Cartridge 416 Y"},{model:"Cartridge 418 BK"},{model:"Cartridge 418 C"},{model:"Cartridge 418 M"},{model:"Cartridge 418 Y"},{model:"Cartridge 069 BK"},{model:"Cartridge 069 C"},{model:"Cartridge 069 M"},{model:"Cartridge 069 Y"},{model:"Cartridge 069H BK"},{model:"Cartridge 069H C"},{model:"Cartridge 069H M"},{model:"Cartridge 069H Y"},{model:"Cartridge 322 Bk"},{model:"Cartridge 322 C"},{model:"Cartridge 322 M"},{model:"Cartridge 322 Y"},{model:"Cartridge 335E Bk"},{model:"Cartridge 335E C"},{model:"Cartridge 335E M"},{model:"Cartridge 335E Y"},{model:"GI-76 BK IN"},{model:"GI-76 C IN"},{model:"GI-76 M IN"},{model:"GI-76 Y IN"},{model:"GI-71 PGBK IN"},{model:"GI-71 C IN"},{model:"GI-71 M IN"},{model:"GI-71 Y IN"},{model:"GI-71S PGBK IN"},{model:"GI-71S C IN"},{model:"GI-71S M IN"},{model:"GI-71S Y IN"},{model:"GI-70 PGBK IN"},{model:"GI-70 C IN"},{model:"GI-70 M IN"},{model:"GI-70 Y IN"},{model:"GI-790 BK IN"},{model:"GI-790 C IN"},{model:"GI-790 M IN"},{model:"GI-790 Y IN"},{model:"PGI-35 BK IN"},{model:"CLI-36 CLR IN"},{model:"PGI-750 PGBK IN"},{model:"CLI-751 C IN"},{model:"CLI-751 M IN"},{model:"CLI-751 Y IN"},{model:"CLI-751 BK IN"},{model:"CLI-751 GY IN"},{model:"PGI-2700 BK XL IN"},{model:"PGI-2700 C XL IN"},{model:"PGI-2700 M XL IN"},{model:"PGI-2700 Y XL IN"},{model:"BH-7 IN"},{model:"CH-7 IN"},{model:"CH-71R IN"},{model:"CH-71L IN"},{model:"BH-70 IN"},{model:"CH-70 IN"},{model:"MC-G01"},{model:"MC-G02"},{model:"MC-G03"},{model:"MC-G04"}]
+
+var personDetail=["Ramlesh Singh","Ranjeet Singh","Anand K Singh","Ankur Goel","Vishesh Kumar Bhatia","Rishabh Kapoor","Manoj Gulati","Abhishek K Singh","Ashutosh Dubey","Ankit Sharma","Yunus Khan","Anupam Mahendru","Virendra Dhariwal","Amit Nanchahal","Vikas Gupta","Manoj Sangar","Vijay Verma","Rahul Dixit","Shivanshu Tyagi","Naheed Akhter","Jaspreet Singh Lamba","Udayan Banerjee","Suvrendu Mitra","Saptarshi Bhandari","Inderjit Yadav","Vir Vijay","Biswajit Ghosh","Sagnik Kar","Pinaki Choudhury","Satender Yadav","Gautam Deb","Dinesh Kumar Singh","Loknath Khanal","Lokesh Nath","H.B Parida","Bharat K SIngh","Pankaj Das","Dipankar Mahanta","Pritam Chakraborty","Rajnish Rai","Kamal Gohil","Sameer Kenia","Rajesh Phatangare","Animesh B","Sanjeev Pathak","Pankaj Shukla","Ashish Fotedar","Kelvin G","Rahim Nadaf","Firoz Sayyad","Lokesh BS","Ajay Chawla","Raj Tiwari","Saurabh Shah","Prashant Bhojne","Sujay Sharma","Rakesh Kaurav","Karan D.","Ratnesh Gautam","Arvind Kalchuri","Brajesh Katare","Abhishek Jadhav","B Dileep","Sanee","Deepu James","Ranadheer Reddy","Veerandra Krishna","Pasupulati Pramod K","Prakash Chandana","Vinay Kumar B R","Snehasis Mohanty","Yuvraj Dilip Hangirgekar","Krishna Kiran","Pinnamaneni Naveen","Sujesh Soman","Jagan George","Rajesh S","Anoop R S","Shankar D","Gangadharan M","Mudari Satyanarayana","Sarath Nath","A Shiva Kumar","Al Subramanian","Yuvaraj S"]
+
+var partnerDetail=["A And S Corporation","Aanchal Sales","Aarohi Enterprises","Aarohi Multitrading & Services","Abhishek Computers & Laptops","Able Technologies","Ace Business Corporation","Aditi Enterprises","Aditya Enterprises","Advance Digital Technology Services","Advance Solutions","Aegis Infoware Pvt Ltd","Alankar Distributors","Alka Corporation","Alpha Ink Private Limited","Amazing Systesms & Technologoies","Amitron Digitech Private Limited","Amoeba Groups","Amrita Infotech","Andhra Stationery Syndicate","Anjali Infocom India","Ankita Electronics","Ansi Computers","Antariksh Enterprises","Apex Corporation India Private Limited","Apex System","Appa Lan Systems Private Limited","Apple Stationery","Appollo Systems & Services","Aquiline Infomedia Pvt Ltd","Arihant Enterprise","Arihant Sales Agency","Artline Solutions","Arya Computech Pvt Ltd","Arya Computech Pvt Ltd/Bengal Tracom Private Limited","Arya Infotech/Bengal Tracom Private Limited","As Technology Pvt Ltd","Ascent Information Systems Private Limited","Asha Infosystems","Avinash Traders","Avnish Traders","Azonic Tech Solutions","Azonic Tech Solutions/Centaur Computer Technology Private Limited","B E Office Automation","B M Paper Agencies","Balaji Enterprises","Bama Infotech","Bena","Bengal Tracom Private Limited","Best Of East Private Limited/Technopolis Dealcom","Best Of East Pvt Ltd","Best Of East Pvt Ltd/Technopolis Dealcom Private Limited","Best Office Solutions","Big C Technologies Pvt Ltd","Bon Moyar Computer","Britex Enterprises","Business Centre- Net","Business Centre- Net/Technopolis Dealcom Private Limited","Byte Infosys","CBS CORPORATE BUSINESS SOLUTION PRIVATE LIMITED","Centaur Computer Technology","Centrium Group","Chawla Digital Systems Pvt Ltd","Commercial Controls","Comnet Vision","Comp Care System( Concept Enterprises & Office Automation)","Compu Graphics Systems","Compulink Info Solution","Computer India","Computer Info Tech Division","Computer Instruments","Computer Media Centre/ Kritika Electronics","Computer Stationery","Compuworld Automation & System Care Pvt Ltd","Compuzer Solutions Pvt Ltd","Concept Enterprises & Office Automation","Copier Corporation Plus","Copier Techno Systems","Copypro Technologies Pvt Ltd","Corenet System","Corporate Business Solution","Cyber Infotech","Cyber King Technologies Pvt Ltd","Cyber King Technologies Pvt Ltd/Super Enterprises","Cyber Peripherals / Sea Techno Solutions","Cyber Peripherals Pvt Ltd","D N Marketing Solutions","Data Trade Products","DD Enterprises","Delite Enterprises","Delite Enterprises/Mannat Enterprises","Delta Business Systems","Devlok Distributors","Digital Infotech Services","Docket Care Systems","Eha Shivam Technologies","Euro Agencies","Excel Marketing Corporation","Excite Inc","Fido Enterprises","Foto Flux Systems Pvt Ltd","G R Enterprises","G R Marketing","Ganesh Computer Services","Garuda Systems","Gateway System","General Peripherals","General Peripherals/S.K Enterprises","Genesis Technologies Services","Girraj Enterprises","Global Infra","Goel Agencies","Gopal Enterprises","Guruji Enterprises","Gurusons Communications Private Limited","H M Solutions","Hbc Computer","Himanshi Enterprises","Hind Digital Private Limited","Hind Digital Solutions","Hind Digital Solutions/Mitushi Electronic Components Pvt. Ltd.","Htl Integrated System","Hurricane Systems Pvt Ltd","I T World - Allahabad","Icon Solutions","Icons Print Media Private Limited","Imperial Techno Industries/Technopolis Dealcom Private Limited","India Marketing","Indoteq Office Automation Systems","Info Edge/Technopolis Dealcom Private Limited","Instant Sales And Service","Integrated System & Services","Irys Systems","IT World","IT Zone","J.P Office Equipment","Jai Jinendra Computers","Jai Maa Trading","Jalan Distributors","JK Services","JM Enterprises","JMD Infotech Private Limited","JN Chunder & Bros.","Jn Chunder & Bros./Business Centre- Net","JRD Services","Jwala Distributors","K A S Printers","K N Computer","Kalapurna Enterprise","Kamal Nayan Marketing Associates Private Limited","Karthikk Systems","Kaushal Enterprises","Kay Pee Enterprises","Kay Pee Enterprises/Britex Enterprises","Krishna Computech","Krishna Consultancy Services","Krishna Corporation","Krishna Enterprises","Kritika Electronics","Kunal Enterprise","Lakhmani Infotech","Lakshya Enterprises","Laser Systems Pvt Ltd","Lustre Softech Private Limited","Lustre Softech Private Limited./M/S Alankar Distributors","M/S Comp Care System","M/s Diya Enterprises","M/S Dreamland","M/S Eha Shivam Technologies","M/S Heemo (India)","M/s I.M Enterprises","M/s IT Care Computers","M/S Rudra Tech Inc.","M/S Shiva Agencies","M/S Sun Corporation","M/S Tawi Sales Corporation","M/S Tritech Enterprises","M/s. Essgi Infotech Pvt. Ltd","M/S. Infotech Solutions & Peripherals","Magneq Systems Pvt Ltd","Mahavir Computers","Mannat Enterprises","Mannat Enterprises/Hind Digital Solutions","Marikar (Motors) Ltd","Market Creast","Master Computers","Matrix Solutions","Mehulendra Corporation","Microware Communications","Minesource India Private Limited","Minitek Systems India Pvt Ltd","Mitushi Electronic Components Pvt. Ltd.","Mksk Office Solutions Llp","MKSK Office Solutions LLP / Delite Enterprises","Mksk Office Solutions Llp/Hind Digital Solutions","MPR Systems","Multi Plus","Multi Sales","Nagpur Computers And Stationers","Nahata Data Products Pvt Ltd","Navaneet Sales","Navaneet Systems","Netizens It Solutions","Neumann Infocom Private Limited","Neuron Techno Solutions","New Way Office Automation Pvt. Ltd.","Offcom Plus","Office Supplies Depot","Om Computers","Om Corporations","Om Infosys","Orient Business Services Pvt Ltd","Orisa Comnet","Orissa Comnet","OST Electronics","Pelikan Office Automation Private Limite","Pentacle IT Solotions Pvt Ltd","Pentacle Solutions Private Limited","Perfect Business System","Photo Care","Pioneer","PN Solutions & Electricals","Power Max","Prakash Teleservices Pvt Ltd","Prince Enterprises","Prithvi It Products Private Limited","Proton Infosys Private Limited","Pv Computer System","R K Enterprises","R M Enterprises","R R Enterprises","R.Viswanathan Veno Agencies","RCS Infotech","Reliable Reprographics","Reliance Systems","Repro Images Solutions","Resilient Infra Solutions","Rhombus Systems","Rialto Enterprise","Rialto Enterprise/JN Chunder & Bros.","Rp Computer","RR Enterprises","S Enterprises","S&S Computertech Private Limited","Sahu Copier Services","Sai Copier Care","Sai Systems & Communications","Saisha Traders","Saitronics Systems Private Limited","San Computek","San Computek /Gateway System","San Computek/P G Enterprises","Sanaaree Computers","Sanidhya Office Automation","Sanmati Office Automation","Sawariya Enterprises","Sea Techno Solutions","Secure And Sound Automation","Sharp Services","Shirish Enterprises","Shiv Telecom","Shree Enterprises","Shree Info System Solutions Private Limited","Shree Office Automation Solutions LLP","Shree Saikripa Commercial Enterprise","Shreekar Enterprises","Shri Sai Ram Enterprises","Shubham Info Tech Systems","Siddharth Industries","Sky Solutions","Slash Computers","Smart Samarth Enterprises","Sparsh Office Solutions","Spectrum Services","Speed Business Machine","Speedwings Solutions","Sre System Private Ltd","Sriram Electronics Private Limited","SS Systems and Solutions","Sumati Alliance","Sun Infotech","Sunglory Multitrade Company","Sunrise Systems","Suntech Infosolutions","Super Enterprises","Super Tech Equipments Pvt Ltd","SVS Technologies","Swaroop Inc","Taneja Photocopiers World","Tangent Solutions","Task Force Computer","Tech Mall","Tech Medic Communications","Techno Planner Llp","Techno Planners LLP","Techno Serva","Technomatrix","Technopolis Dealcom Private Limited","Techspert","The Matrix Infotech Paradims","Trend Marketing","Twin City Enterprises","Uneecops Technologies Limited","Uneecops Technologies Limited/Hind Digital Solutions","Unicom Infotel Pvt Ltd","Unique Computers & Stationers","Unique Infosales","Unique Media Services","Unique Stationers","United Copier","Universal Business Systems","Uttaranchal Sales","V.S.N International","Varay Image Runners","Veera Trading Company","Venkatesh It Solutions Private Limited","Venus Technical Systems","Vibrant Infotech","Vijay Stationery Mart","Vikel Business Corporation","Virtual Technologies","Vishal Infotech","Vision Infotech","Vp Sales And Servicing","Vtek Traders","We Enterprises","Worldcom Computer Technologies","Xerobit Corporation","Xerobit Ventures India Pvt Ltd","Zion Digital Technologies Private Limited"];
+
+
+class OptionAdder {
+  constructor(partnerDetailBox, personDetailBox, modelNameBox) {
+    this.partnerDetailBox = partnerDetailBox;
+    this.personDetailBox = personDetailBox;
+    this.modelNameBox = modelNameBox;
+    this.addElementToBox(partnerDetail, partnerDetailBox, this.optionCreate);
+    this.addElementToBox(personDetail, personDetailBox, this.optionCreate);
+    this.addElementToBox(modelName, modelNameBox, this.labelCreate);
+  }
+
+  optionCreate(value) {
+    const optionElement = document.createElement("option");
+    optionElement.value = value;
+    optionElement.text = value;
+    return optionElement;
+  }
+
+  labelCreate(value) {
+    const labelElement = document.createElement("label");
+    labelElement.setAttribute('for', value.model);
+
+    const inputElement = document.createElement('input');
+    inputElement.setAttribute('type', 'checkbox');
+    inputElement.setAttribute('id', value.model);
+    inputElement.setAttribute('value', value.model);
+
+    const textNode = document.createTextNode(value.model);
+
+    labelElement.appendChild(inputElement);
+    labelElement.appendChild(textNode);
+    document.getElementById("checkboxes").appendChild(labelElement);
+    return labelElement;
+  }
+
+  addElementToBox(arrayValue, whereToAddBox, createFunction) {
+    arrayValue.forEach((value) => {
+      const element = createFunction(value);
+      whereToAddBox.appendChild(element);
+    });
+  }
+}
+
+// Object creation
+const addElement = new OptionAdder(
+  document.getElementById("partnerid"),
+  document.getElementById("sales_person"),
+  document.getElementById("checkboxes")
+);
 class Multiselect {
   constructor(selectBoxInput,multiselectElement, checkboxesElement, cpcpInputElement, gemCpcpSelectElement) {
     this.selectBoxInput = selectBoxInput;
@@ -7,11 +62,12 @@ class Multiselect {
     this.gemCpcpSelectElement = gemCpcpSelectElement;
     this.upIcon = multiselectElement.querySelector(".fa-angle-up");
     this.downIcon = multiselectElement.querySelector(".fa-angle-down");
+    this.modelInput=document.getElementById("modelNameInput");
     this.isCheckboxesVisible = false;
     this.selectBoxInput.addEventListener("click", this.handleClickEvent.bind(this));
     document.body.addEventListener("click", this.handleClickOutsideEvent.bind(this));
     this.gemCpcpSelectElement.addEventListener("change", this.handleSelectChangeEvent.bind(this));
-    modelNameInput.addEventListener("input", this.handleModelNameInputChange.bind(this));
+    this.modelInput.addEventListener("input", this.handleModelNameInputChange.bind(this));
   }
 
   handleClickEvent(event) {
@@ -26,6 +82,8 @@ class Multiselect {
   handleClickOutsideEvent(event) {
     if (!this.multiselectElement.contains(event.target)) {
       this.hideCheckboxes();
+      this.modelInput.value="";
+      this.handleModelNameInputChange({ target: { value: "" } });
     }
   }
 
@@ -78,7 +136,7 @@ const multiselect = new Multiselect(
 
 //--------------------------------//
 class InputRowManager {
-  constructor(checkboxesElement, appendNewFieldElement, selectPossible = 3) {
+  constructor(checkboxesElement, appendNewFieldElement, selectPossible = 10) {
     this.checkboxesElement = checkboxesElement;
     this.appendNewFieldElement = appendNewFieldElement;
     this.defaultSelectPossible=selectPossible;
@@ -94,17 +152,13 @@ class InputRowManager {
         this.selectPossible--;
         this.createInputRow(targetElement.id, targetElement.value);
       } else if (this.selectPossible > 0) {
-        if (this.removeInputRow(targetElement.id)) {
-          this.selectPossible++;
-        }
+        this.removeInputRow(targetElement.id)        
       } else {
         if(targetElement.checked){
           alert(`You can't select more than ${this.defaultSelectPossible} models`);
         }
         targetElement.checked = false;
-        if (this.removeInputRow(targetElement.id)) {
-          this.selectPossible++;
-        }
+        this.removeInputRow(targetElement.id)
       }
     }
   }
@@ -141,9 +195,8 @@ class InputRowManager {
     const rowToRemove = document.getElementById(`row_${id}`);
     if (rowToRemove) {
       this.appendNewFieldElement.removeChild(rowToRemove);
-      return true;
+      this.selectPossible++;
     }
-    return false;
   }
 }
 //object created
@@ -151,8 +204,5 @@ const inputRowManager = new InputRowManager(
   document.getElementById("checkboxes"),
   document.querySelector("#append_new_field")
 );
-
-
-
 
 
---
+++ jspractice/script_auto_complete.js
@@ -0,0 +1,96 @@
+var personDetail=["Ramlesh Singh","Ranjeet Singh","Anand K Singh","Ankur Goel","Vishesh Kumar Bhatia","Rishabh Kapoor","Manoj Gulati","Abhishek K Singh","Ashutosh Dubey","Ankit Sharma","Yunus Khan","Anupam Mahendru","Virendra Dhariwal","Amit Nanchahal","Vikas Gupta","Manoj Sangar","Vijay Verma","Rahul Dixit","Shivanshu Tyagi","Naheed Akhter","Jaspreet Singh Lamba","Udayan Banerjee","Suvrendu Mitra","Saptarshi Bhandari","Inderjit Yadav","Vir Vijay","Biswajit Ghosh","Sagnik Kar","Pinaki Choudhury","Satender Yadav","Gautam Deb","Dinesh Kumar Singh","Loknath Khanal","Lokesh Nath","HB Parida","Bharat K SIngh","Pankaj Das","Dipankar Mahanta","Pritam Chakraborty","Rajnish Rai","Kamal Gohil","Sameer Kenia","Rajesh Phatangare","Animesh B","Sanjeev Pathak","Pankaj Shukla","Ashish Fotedar","Kelvin G","Rahim Nadaf","Firoz Sayyad","Lokesh BS","Ajay Chawla","Raj Tiwari","Saurabh Shah","Prashant Bhojne","Sujay Sharma","Rakesh Kaurav","Karan D","Ratnesh Gautam","Arvind Kalchuri","Brajesh Katare","Abhishek Jadhav","B Dileep","Sanee","Deepu James","Ranadheer Reddy","Veerandra Krishna","Pasupulati Pramod K","Prakash Chandana","Vinay Kumar B R","Snehasis Mohanty","Yuvraj Dilip Hangirgekar","Krishna Kiran","Pinnamaneni Naveen","Sujesh Soman","Jagan George","Rajesh S","Anoop R S","Shankar D","Gangadharan M","Mudari Satyanarayana","Sarath Nath","A Shiva Kumar","Al Subramanian","Yuvaraj S"]
+
+
+class Node {
+   constructor(children = new Array(27).fill(null), endOfWord = false) {
+      this.children = children;
+      this.endOfWord = endOfWord;
+   }
+}
+
+class Tree {
+   constructor(stroage) {
+      this.root = new Node();
+      this.allWords = [];
+      this.stroage=stroage;   
+      this.createTree();   
+   }
+
+   insert(word) {
+      let temp = this.root;
+      word = word.toLowerCase();
+      for (let char of word) {
+         let index;
+         if (char === ' ') {
+            index = 26; 
+         } else {
+            index = char.charCodeAt(0) - 97;
+         }
+         if (temp.children[index] === null) {
+            temp.children[index] = new Node();
+         }
+         temp = temp.children[index];
+      }
+      temp.endOfWord = true;
+   }
+
+   searchWithPrefixExist(prefix) {
+      let temp = this.root;
+      this.allWords = [];
+      for (let char of prefix) {
+         let index;
+         if (char === ' ') {
+            index = 26; 
+         } else {
+            index = char.charCodeAt(0) - 97;
+         }
+         if (temp.children[index] === null) {
+            return [];
+         }
+         temp = temp.children[index];
+      }
+      this.allWordsWithPrefix(prefix, temp);
+      return this.allWords;
+   }
+
+   allWordsWithPrefix(word, lastNode) {
+      if (lastNode.endOfWord) {
+         this.allWords.push(word);
+      }
+      for (let i = 0; i < lastNode.children.length; i++) {
+         let nodeExist = lastNode.children[i];
+         if (nodeExist !== null) {
+            if (i === 26) {
+               this.allWordsWithPrefix(word + ' ', nodeExist);
+            } else {
+               this.allWordsWithPrefix(word + String.fromCharCode(i + 97), nodeExist);
+            }
+         }
+      }}
+   createTree(){
+      for(let i=0;i<this.stroage.length;i++){
+         this.insert(this.stroage[i]);
+      }
+   }
+}
+
+
+var createSearchTree=new Tree(personDetail);
+
+const input_search=document.getElementById("inputSearch");
+input_search.addEventListener("input",function(){
+   var search_result=createSearchTree.searchWithPrefixExist(input_search.value);
+   if(search_result.length>0){
+      document.getElementById("searchResult").innerHTML=search_result.join("<br>");
+   }
+   else{
+      document.getElementById("searchResult").innerHTML="No Result Found";
+   }
+
+})
+
+
+
+
+
+
