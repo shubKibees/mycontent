e3c3789437093e7f06106eb8161d0e9e712be6b8
shubKibees <shubhamkibees@gmail.com>
2023-11-07 16:16:28 +0530

    update bubble

--- jspractice/script_grid.js
+++ jspractice/script_grid.js
@@ -1,67 +1,87 @@
-const grid_cont=document.querySelector(".grid")
-const container=document.querySelector(".container")
-function div_create(text,size){
-    const div=document.createElement("div")
-    div.classList.add("grid-item")
-    div.innerHTML=text
-    div.style.width=`${block_square}px`
-   div.style.height=`${block_square}px`
-   div.classList.add("block");
-   div.setAttribute(`data-position`,index);
-   div.style.backgroundColor=`rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
-    return div    
+const gridContainer = document.querySelector(".grid");
+const container = document.querySelector(".container");
+
+const BLOCK_SIZE = 100;
+
+let noOfBlocksInRow = 0;
+
+function createGridItem(index) {
+    const div = document.createElement("div");
+    div.classList.add("grid-item");
+    div.innerHTML = `${index}`;
+    div.style.width = `${BLOCK_SIZE}px`;
+    div.style.height = `${BLOCK_SIZE}px`;
+    div.classList.add("block");
+    div.dataset.position = index;
+    div.style.backgroundColor = randomColor();
+    return div;
 }
 
-var width=container.getBoundingClientRect().width;
-var height=container.getBoundingClientRect().height;
-var index=0;
-var no_of_blocks_in_row=0;
-const block_square=100;
-while(width>=0||height>=0){
-   const div=div_create(`${index}`,block_square)
-   grid_cont.appendChild(div)
-   index++;
-   width-=block_square;
-   if(width<=0&&height>=0){
-      height-=block_square;
-      if(!no_of_blocks_in_row){
-         no_of_blocks_in_row=index-1;
-      }
-      width=container.getBoundingClientRect().width-block_square;
-   }
+function randomColor() {
+    return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
 }
 
-function findDimension(origin,no_of_block_within_row){
-   let x=origin%no_of_block_within_row;
-   let y=Math.floor(origin/no_of_block_within_row);
-   let top=(y-1)*no_of_block_within_row+x;
-   let bottom=(y+1)*no_of_block_within_row+(x);
-   let left=y*no_of_block_within_row+(x-1);
-   let right=y*no_of_block_within_row+(x+1);
-   return[
-      origin,top,bottom,left,right]
+function createGrid() {
+    let width = container.getBoundingClientRect().width;
+    let height = container.getBoundingClientRect().height;
+    let index = 0;
+
+    while (width >= 0 || height >= 0) {
+        const gridItem = createGridItem(index);
+        gridContainer.appendChild(gridItem);
+        index++;
+        width -= BLOCK_SIZE;
+
+        if (width <= 0 && height >= 0) {
+            height -= BLOCK_SIZE;
+            if (!noOfBlocksInRow) {
+                noOfBlocksInRow = index - 1;
+            }
+            width = container.getBoundingClientRect().width - BLOCK_SIZE;
+        }
+    }
 }
 
-container.addEventListener("mouseover", (e) => {
-   if (e.target.classList.contains("block")) {
-      let dimension = findDimension(e.target.getAttribute("data-position"), no_of_blocks_in_row);
-      console.log(`dimension: ${dimension}`)
-      for (let index of dimension) {
-         let element = document.querySelector(`[data-position="${index}"]`);
-         element.classList.add("animate"); 
-      }
-   }
-});
+function handleResize() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    createGrid();
+}
 
-container.addEventListener("mouseout", (e) => {
-   if (e.target.classList.contains("block")) {
-      let dimension = findDimension(e.target.getAttribute("data-position"), no_of_blocks_in_row);
-      for (let index of dimension) {
-         let element = document.querySelector(`[data-position="${index}"]`);
-         element.classList.remove("animate"); 
-      }
-   }
-});
+function findDimension(origin, noOfBlocksWithinRow) {
+    const x = origin % noOfBlocksWithinRow;
+    const y = Math.floor(origin / noOfBlocksWithinRow);
+    const top = (y - 1) * noOfBlocksWithinRow + x + 1;
+    const bottom = (y + 1) * noOfBlocksWithinRow + x - 1;
+    const left = y * noOfBlocksWithinRow + (x - 1);
+    const right = y * noOfBlocksWithinRow + (x + 1);
+    return [origin, top, bottom, left, right];
+}
 
+function handleMouseOver(e) {
+    if (e.target.classList.contains("block")) {
+        const dimension = findDimension(e.target.dataset.position, noOfBlocksInRow);
+        console.log(dimension);
+        dimension.forEach(index => {
+            const element = document.querySelector(`[data-position="${index}"]`);
+            element && element.classList.add("animate");
+        });
+    }
+}
 
+function handleMouseOut(e) {
+    if (e.target.classList.contains("block")) {
+        const dimension = findDimension(e.target.dataset.position, noOfBlocksInRow);
+        dimension.forEach(index => {
+            const element = document.querySelector(`[data-position="${index}"]`);
+            element && element.classList.remove("animate");
+        });
+    }
+}
 
+window.addEventListener("resize", handleResize);
+container.addEventListener("mouseover", handleMouseOver);
+container.addEventListener("mouseout", handleMouseOut);
+
+createGrid();
